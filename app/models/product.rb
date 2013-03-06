64cdd0d022620546ba0dce1072d859e28be1023a
gfrikpost <gfrikpost@gmail.com>
2013-03-06 10:37:28 +0400

    Improved display of products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title
+  
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
@@ -8,4 +9,6 @@ class Product < ActiveRecord::Base
     message: 'must be a URL for GIF, JPG or PNG image.'
               # URL должен указывать на изображение формата GIF, JPG или PNG
   }
+  
+  validates :title, :length => { :minimum => 10 }
 end
--- test/functional/products_controller_test.rb
+++ test/functional/products_controller_test.rb
@@ -15,6 +15,7 @@ class ProductsControllerTest < ActionController::TestCase
     get :index
     assert_response :success
     assert_not_nil assigns(:products)
+    assert_select '.list_actions a', 'Edit'
   end
 
   test "should get new" do
