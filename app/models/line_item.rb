e87ddd2b5decd9aa41559f11de7d55415b48e585
gfrikpost <gfrikpost@gmail.com>
2013-03-21 17:37:17 +0400

    model order

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,6 +1,7 @@
 class LineItem < ActiveRecord::Base
-  attr_accessible :cart_id, :product_id, :quantity, :price
+  attr_accessible :cart_id, :product_id, :quantity, :price, :product, :cart
   
+  belongs_to :order
   belongs_to :product
   belongs_to :cart
   
