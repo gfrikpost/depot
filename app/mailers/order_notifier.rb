98bd5a1ff9b266a243c424a4e77046d09bc215de
gfrikpost <gfrikpost@gmail.com>
2013-03-25 09:45:19 +0400

    mailer from home

---
+++ app/mailers/order_notifier.rb
@@ -0,0 +1,25 @@
+class OrderNotifier < ActionMailer::Base
+  default from: "gfrikpost@gmail.com"
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_notifier.received.subject
+  #
+  def received(order)
+    @order = order
+    
+    mail to: order.email, subject: 'confirmation of an order Pragmatic Store'
+  end
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_notifier.shipped.subject
+  #
+  def shipped(order)
+    @order = order
+
+    mail to: order.email, subject: 'order from Pragmatic Store has been shipped'
+  end
+end
