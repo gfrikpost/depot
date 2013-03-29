1f28430bb884231465e72d606164623b5e487753
gfrikpost <gfrikpost@gmail.com>
2013-03-29 12:56:34 +0400

    Well done

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,9 +2,13 @@ class StoreController < ApplicationController
   skip_before_filter :authorize
   
   def index
-    @products = Product.order(:title)
-    @cart = current_cart
-    @time = Time.now
+    if params[:set_locale]
+      redirect_to store_path(locale: params[:set_locale])
+    else
+      @products = Product.order(:title)
+      @cart = current_cart
+      @time = Time.now
+    end
     
     if session[:counter].nil?
       session[:counter] = 0
